9b87c1b1e2734487951dfa00d1e4bc36d9ea0d21
Anna Peterson <reveragb@gmail.com>
2022-12-12 16:33:10 -0700

    Unify code structure

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -2,19 +2,19 @@
 
 class UserRepository {
   constructor(data) {
-    this.data = data;
+    this.data = data
   }
 
   getData(userID) {
-    return this.data.userData.find(currentUser => currentUser.id === userID);
+    return this.data.userData.find(currentUser => currentUser.id === userID)
   }
 
   stepGoalAverage() {
     let sum = this.data.userData.reduce((acc, user) => {
-      acc += user.dailyStepGoal;
-      return acc;
-    }, 0);
-    return parseInt(sum / this.data.userData.length);
+      acc += user.dailyStepGoal
+      return acc
+    }, 0)
+    return parseInt(sum / this.data.userData.length)
   }
 }
 
