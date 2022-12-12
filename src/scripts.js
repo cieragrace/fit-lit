80db4a7b6f592217cb4d8ab11e79ee6abb83ab2e
Anna Peterson <reveragb@gmail.com>
2022-12-12 16:37:36 -0700

    Delete semicolons

--- src/scripts.js
+++ src/scripts.js
@@ -39,67 +39,67 @@ function displayUserInfo() {
 }
 
 function displayWelcomeName() {
-  activityTrackerTitle.innerText += ` ${currentUser.getFirstName()}`;
+  activityTrackerTitle.innerText += ` ${currentUser.getFirstName()}`
 }
 
 function displayStepGoal() {
-  return [currentUser.userData.dailyStepGoal, (users.stepGoalAverage() - currentUser.userData.dailyStepGoal)];
+  return [currentUser.userData.dailyStepGoal, (users.stepGoalAverage() - currentUser.userData.dailyStepGoal)]
 }
 
 // Functions
 function getUser(sleep, hydration) {
-  let randomIndex = Math.floor(Math.random() * users.data.userData.length);
-  let randomUser = users.data.userData[randomIndex];
-  currentUser = new User(randomUser, sleep, hydration);
+  let randomIndex = Math.floor(Math.random() * users.data.userData.length)
+  let randomUser = users.data.userData[randomIndex]
+  currentUser = new User(randomUser, sleep, hydration)
 }
 
 function getUserFriends() {
   return currentUser.userData.friends
     .map(friend => users.getData(friend).name)
-    .join(', ');
+    .join(', ')
 }
 
 function displayWater() {
-  let dailyWaterIntake = currentUser.getWaterPerDay('2019/06/15');
-  return [dailyWaterIntake, 96 - dailyWaterIntake];
+  let dailyWaterIntake = currentUser.getWaterPerDay('2019/06/15')
+  return [dailyWaterIntake, 96 - dailyWaterIntake]
 }
 
 function displaySleep() {
-  let dailySleep = currentUser.sleepOnSpecificDate('2019/06/15');
-  return [dailySleep, 12 - dailySleep];
+  let dailySleep = currentUser.sleepOnSpecificDate('2019/06/15')
+  return [dailySleep, 12 - dailySleep]
 }
 
 function displaySleepQuality() {
-  let dailyQuality = currentUser.sleepQualityOnSpecificDate('2019/06/15');
-  return [dailyQuality, 5 - dailyQuality];
+  let dailyQuality = currentUser.sleepQualityOnSpecificDate('2019/06/15')
+  return [dailyQuality, 5 - dailyQuality]
 }
 
 function displayLast7DaysSleep() {
   return currentUser
     .givenWeekSleepDataByDay()
-    .map(current => Object.values(current)[0]);
+    .map(current => Object.values(current)[0])
 }
 
 function displayLast7DaysQuality() {
   return currentUser
     .givenWeeksSleepQualityByDay()
-    .map(current => Object.values(current)[0]);
+    .map(current => Object.values(current)[0])
 }
 
 function displayLast7DaysHydration() {
   return currentUser
     .getWeeklyConsumption()
-    .map(current => Object.values(current)[0]);
+    .map(current => Object.values(current)[0])
 }
 
 function displayAllTimeSleepData() {
-  return [currentUser.getOverallQualityAvg(), currentUser.getAverageDailySleep()];
+  return [currentUser.getOverallQualityAvg(), currentUser.getAverageDailySleep()]
 }
 
 function loadPage() {
-  getUser(sleep, hydration);
-  displayUserInfo();
-  displayWelcomeName();
+  getUser(sleep, hydration)
+  displayUserInfo()
+  displayWelcomeName()
   loadCharts(displayStepGoal(),
     displayWater(),
     displaySleep(),
@@ -107,5 +107,5 @@ function loadPage() {
     displayLast7DaysSleep(),
     displayLast7DaysQuality(),
     displayLast7DaysHydration(),
-    displayAllTimeSleepData());
+    displayAllTimeSleepData())
 }
